65ba34545de451f8eccbf44403e71bd70a736a27
Ilon Sjögren <ilon.sjogren@viaplay.com>
2016-05-10 06:30:45 +0200

    Should verify that sync target exists and restrict which users to run for

--- unison.py
+++ unison.py
@@ -21,7 +21,11 @@ class ConfigurationException(Exception):
     pass
 
 
-class InvalidSyncTargetException(ConfigurationException):
+class ConfigurationNotFoundException(ConfigurationException):
+    pass
+
+
+class InvalidUnisonTargetException(ConfigurationException):
     pass
 
 
@@ -30,19 +34,32 @@ class InvalidSyncTargetException(ConfigurationException):
 USER_CONFIG_PATH = os.path.sep + os.path.join('Users', '{USER}', 'Library', 'Application Support', 'Unison')
 TEMPLATE_CONFIG_PATH = os.path.sep + os.path.join('Library', 'TT', 'Config', 'Unison')
 
+# Target sync directory, this directory must exists.
+SYNC_TARGET_PATH = os.path.sep + os.path.join('Volumes', '{USER}')
+
 # Override config for testing purpose
 # noinspection PyRedeclaration
 # TEMPLATE_CONFIG_PATH = os.path.join('TT', 'Config', 'Unison')
 
 # Extra arguments to pass to Unison
-UNISON_EXTRA_ARGS = ['-silent']
+UNISON_EXTRA_ARGS = [
+    '-silent',
+]
+
+# Exit codes:
+# 0:     Script executed with no errors
+# 1 - 3: Unison exit codes
+# 11:    User not allowed to run sync
+# 12:    Sync target path does not exist
+# 13:    Invalid Unison target config
+# 14:    Could not find configuration file
 
 # Non-fatal exit codes from unison that the script should ignore
 IGNORED_EXIT_CODES = [
-    # 0  # successful synchronization; everything is up-to-date now.
-    1,  # some files were skipped, but all file transfers were successful.
-    2,  # non-fatal failures occurred during file transfer.
-    # 3  # a fatal error occurred, or the execution was interrupted.
+    # 0   # successful synchronization; everything is up-to-date now.
+    # 1,  # some files were skipped, but all file transfers were successful.
+    # 2,  # non-fatal failures occurred during file transfer.
+    # 3   # a fatal error occurred, or the execution was interrupted.
 ]
 
 TEMPLATE_EXTENSION = 'prfconfig'
@@ -60,38 +77,51 @@ TEMPLATE_SHARED_CONFIG = 'Common'
 # Lowest numerical ID on the system that sync should be run for
 LOWEST_ALLOWED_USER_ID = 501
 # Explicit list of users not to run sync for
-PROHIBITED_SYNC_USERS = ['root']
+PROHIBITED_SYNC_USERS = [
+    'root',
+]
 
 
 def get_current_user_stat():
-    return os.lstat("/dev/console")
+    user_stat = os.lstat("/dev/console")
+    return user_stat.st_uid, pwd.getpwuid(user_stat.st_uid).pw_name
 
 
 def unison_sync(user, target):
     unison_cmd = subprocess.check_output(['which', 'unison']).strip('\n')
 
     if not valid_sync_target(target):
-        raise InvalidSyncTargetException('Not a valid sync target: {}'.format(target))
-
-    create_user_config(username=user, target=target)
+        raise InvalidUnisonTargetException('Not a valid sync target: {}'.format(target))
 
     try:
-        output = subprocess.check_output([unison_cmd, target] + UNISON_EXTRA_ARGS, stderr=subprocess.STDOUT)
-        return output
-    except subprocess.CalledProcessError, e:
-        print(e)
-        raise UnisonSyncException(message=e.message, exit_code=e.returncode)
+        config_path = create_user_config(username=user, target=target)
+        print('Created config: {path}'.format(path=config_path))
+    except IOError as e:
+        if e.errno == 2:
+            raise ConfigurationNotFoundException(
+                'Could not find configuration file for target {}: {}'.format(target, e.filename)
+            )
+        raise e
+
+    try:
+        return subprocess.check_output([unison_cmd, target] + UNISON_EXTRA_ARGS, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise UnisonSyncException(
+            message='Unison `{command}` returned error: \n{error}'.format(command=e.cmd, error=e.output),
+            exit_code=e.returncode,
+        )
 
 
 def valid_sync_target(target):
     return target in TEMPLATE_CONFIG_TARGETS
 
 
-def valid_sync_user(user_id):
-    user_name = pwd.getpwuid(user_id)
-    if user_id >= LOWEST_ALLOWED_USER_ID and user_name not in PROHIBITED_SYNC_USERS:
-        return True
-    return False
+def valid_sync_user(uid, name):
+    if uid < LOWEST_ALLOWED_USER_ID:
+        return False
+    elif name in PROHIBITED_SYNC_USERS:
+        return False
+    return True
 
 
 def create_user_config(username, target):
@@ -129,25 +159,33 @@ def create_user_config(username, target):
     return config_path
 
 def main():
-    user_stat = get_current_user_stat()
-    user_id = user_stat.st_uid
-    user_name = pwd.getpwuid(user_id).pw_name
+    user_id, user_name = get_current_user_stat()
 
-    if not valid_sync_user(user_id):
+    if not valid_sync_user(uid=user_id, name=user_name):
         print('Sync should not run for user: {user} ({id})'.format(user=user_name, id=user_id))
-        sys.exit(0)
+        sys.exit(11)
 
-    # Still no check that sync target directory actually exists
-    # This should be performed for each target, on both root's.
+    # This should preferably be done based on the actual target in the configs written.
+    sync_target = SYNC_TARGET_PATH.format(USER=user_name)
+    if not os.path.isdir(sync_target):
+        print('Target sync directory does not exist: {}'.format(sync_target))
+        sys.exit(12)
 
     for target in iter(TEMPLATE_CONFIG_TARGETS):
         try:
+            print('Running sync for {}'.format(target))
             unison_sync(user=user_name, target=target)
         except UnisonException as e:
             if e.exit_code not in IGNORED_EXIT_CODES:
-                print('Unison exited with error {code}, aborting sync'.format(code=e.exit_code))
+                print('Unison exited with error {code}, aborting sync:\n{msg}'.format(code=e.exit_code, msg=e.message))
                 sys.exit(e.exit_code)
             pass
+        except InvalidUnisonTargetException:
+            print('Did not provide a valid Unison target config: {}'.format(target))
+            sys.exit(13)
+        except ConfigurationException as e:
+            print(e.message)
+            sys.exit(14)
 
 if __name__ == "__main__":
     main()
